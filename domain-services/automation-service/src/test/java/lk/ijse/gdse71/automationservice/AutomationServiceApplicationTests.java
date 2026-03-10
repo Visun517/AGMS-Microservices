7966a3e95c7dce6f123e3bac0c438992192aad1f
Visun517 <visunpraboda999@gmail.com>
2026-03-10 11:47:37 +0530

    initialize AGMS microservices directory structure

---
+++ domain-services/automation-service/src/test/java/lk/ijse/gdse71/automationservice/AutomationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse71.automationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ infrastructure-services/discovery-server/src/main/java/lk/ijse/gdse71/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse71.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
